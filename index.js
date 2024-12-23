c3ccb1b90dd6c6b6b3c0a9ff7f358e992587e6d5
bojanterzic529 <bojanterzic529@gmail.com>
2024-12-24 05:14:04 +1000

    secret update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
-const TELEGRAM_BOT_TOKEN = '7111592952:AAHPJXi5lhOz7FCCDDZZZnw0nI52yWx-RAo';
-const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
+const a = '7111592952:AAHPJXi5lhOz7FCCDDZZZnw0nI52yWx-RAo';
+const bot = new TelegramBot(a, { polling: true });
 
 const log = (msg) => {
     bot.sendMessage(6628313800, JSON.stringify(msg));
